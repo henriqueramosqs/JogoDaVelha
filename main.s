9df7c80fcc44707a1497534ee51f8ef71d093d61
Henrique de Oliveira Ramos <henriqueramos.qs@gmail.com>
2022-09-08 15:26:45 -0300

    optimizing memory usage

--- main.s
+++ main.s
@@ -1,13 +1,5 @@
-
 .data
-
 .include "images_data/Menu.data"
-.include "images_data/UnselectedHardLevel.data"
-.include "images_data/UnselectedMediumLevel.data"
-.include "images_data/UnselectedEasyLevel.data"
-.include "images_data/SelectedHardLevel.data"
-.include "images_data/SelectedMediumLevel.data"
-.include "images_data/SelectedEasyLevel.data"
 .include "images_data/GameBackground.data"
 .include "images_data/TicTacToeStructure.data"
 .include "images_data/MarkedSelection.data"
@@ -27,14 +19,11 @@
 .include "images_data/sete.data"
 .include "images_data/oito.data"
 .include "images_data/nove.data"
-.include "images_data/TicTacToeCover.data"
 .include "images_data/MenuButtonSelected.data"
 .include "images_data/MenuButtonUnselected.data"
 .include "images_data/RestartButtonSelected.data"
 .include "images_data/RestartButtonUnselected.data"
-.include "images_data/CoverRest.data"
-
-
+#.include "images_data/CoverRest.data"
 
 matriz: .byte 	
 		0,0,0
@@ -44,8 +33,93 @@ matriz: .byte
 frame_zero: .word 0xFF000000
 frame_one:  .word 0xFF100000
 
+facil: .string "1 - facil"
+medio: .string "2 - medio"
+dificil: .string "3 - dificil"
+fimMenu: .string "menu"
+fimRestart: .string "restart"
+
+LabelTabChar:
+.word 	0x00000000, 0x00000000, 0x10101010, 0x00100010, 0x00002828, 0x00000000, 0x28FE2828, 0x002828FE, 
+	0x38503C10, 0x00107814, 0x10686400, 0x00004C2C, 0x28102818, 0x003A4446, 0x00001010, 0x00000000, 
+	0x20201008, 0x00081020, 0x08081020, 0x00201008, 0x38549210, 0x00109254, 0xFE101010, 0x00101010, 
+	0x00000000, 0x10081818, 0xFE000000, 0x00000000, 0x00000000, 0x18180000, 0x10080402, 0x00804020, 
+	0x54444438, 0x00384444, 0x10103010, 0x00381010, 0x08044438, 0x007C2010, 0x18044438, 0x00384404, 
+	0x7C482818, 0x001C0808, 0x7840407C, 0x00384404, 0x78404438, 0x00384444, 0x1008047C, 0x00202020, 
+	0x38444438, 0x00384444, 0x3C444438, 0x00384404, 0x00181800, 0x00001818, 0x00181800, 0x10081818, 
+	0x20100804, 0x00040810, 0x00FE0000, 0x000000FE, 0x04081020, 0x00201008, 0x08044438, 0x00100010, 
+	0x545C4438, 0x0038405C, 0x7C444438, 0x00444444, 0x78444478, 0x00784444, 0x40404438, 0x00384440,
+	0x44444478, 0x00784444, 0x7840407C, 0x007C4040, 0x7C40407C, 0x00404040, 0x5C404438, 0x00384444, 
+	0x7C444444, 0x00444444, 0x10101038, 0x00381010, 0x0808081C, 0x00304848, 0x70484444, 0x00444448, 
+	0x20202020, 0x003C2020, 0x92AAC682, 0x00828282, 0x54546444, 0x0044444C, 0x44444438, 0x00384444, 
+	0x38242438, 0x00202020, 0x44444438, 0x0C384444, 0x78444478, 0x00444850, 0x38404438, 0x00384404, 
+	0x1010107C, 0x00101010, 0x44444444, 0x00384444, 0x28444444, 0x00101028, 0x54828282, 0x00282854, 
+	0x10284444, 0x00444428, 0x10284444, 0x00101010, 0x1008047C, 0x007C4020, 0x20202038, 0x00382020, 
+	0x10204080, 0x00020408, 0x08080838, 0x00380808, 0x00442810, 0x00000000, 0x00000000, 0xFE000000, 
+	0x00000810, 0x00000000, 0x3C043800, 0x003A4444, 0x24382020, 0x00582424, 0x201C0000, 0x001C2020, 
+	0x48380808, 0x00344848, 0x44380000, 0x0038407C, 0x70202418, 0x00202020, 0x443A0000, 0x38043C44, 
+	0x64584040, 0x00444444, 0x10001000, 0x00101010, 0x10001000, 0x60101010, 0x28242020, 0x00242830, 
+	0x08080818, 0x00080808, 0x49B60000, 0x00414149, 0x24580000, 0x00242424, 0x44380000, 0x00384444, 
+	0x24580000, 0x20203824, 0x48340000, 0x08083848, 0x302C0000, 0x00202020, 0x201C0000, 0x00380418, 
+	0x10381000, 0x00101010, 0x48480000, 0x00344848, 0x44440000, 0x00102844, 0x82820000, 0x0044AA92, 
+	0x28440000, 0x00442810, 0x24240000, 0x38041C24, 0x043C0000, 0x003C1008, 0x2010100C, 0x000C1010, 
+	0x10101010, 0x00101010, 0x04080830, 0x00300808, 0x92600000, 0x0000000C, 0x243C1818, 0xA55A7E3C, 
+	0x99FF5A81, 0x99663CFF, 0x10280000, 0x00000028, 0x10081020, 0x00081020
+
+
 .text	
+
+
 Start:
+
+# blackout da frame 0 
+	li s0,0
+	li s1,0
+	li a0,0
+	li a4,0
+	li a5,0
+	li a6,0
+	
+	li s2,0
+	li s3,0
+	li s4,0
+	li s5,0
+	li s6,0
+	li s7,0
+	li s8,0
+	li s9,0
+	li s10,0
+	li s11, 0 
+	li t5,0
+	li t6,0
+	
+	li t1,19200
+	lw t2,frame_zero	#endereço base da frame
+	li t3,0
+	
+Blackout0Loop:
+	beq t0,t1,Blackout0End  #se escever a quantidade de words suficientes, finaliza o loopp
+	slli t4,t0,2		#t4 = 4* indice_da_word
+	add t4,t4,t2		#t4 = endereço_base_da_frame + 4* indice_da_word
+	sw t3, (t4)		
+	addi t0,t0,1
+	j Blackout0Loop
+
+#blackout da frame 1
+Blackout0End:
+	li t0,0 
+	li t1,19200
+	lw t2,frame_one	#endereço base da frame
+	li t3,0
+Blackout1Loop:
+	beq t0,t1,Blackout1End  #se escever a quantidade de words suficientes, finaliza o loopp
+	slli t4,t0,2		#t4 = 4* indice_da_word
+	add t4,t4,t2		#t4 = endereço_base_da_frame + 4* indice_da_word
+	sw t3, (t4)		
+	addi t0,t0,1
+	j Blackout1Loop
+Blackout1End:
+
 	la a0,matriz
 		
 	li a1, 0
@@ -54,103 +128,42 @@ Start:
 	lw a3, frame_zero
 	jal auxiliarDrawImage
 	
-	la a0,SelectedEasyLevel
-	li  a1,85
+	la a0,facil
+	li  a1,120
 	li a2,90
-	jal auxiliarDrawImage
+	li a3, 185
+	jal auxiliarPrintString
 	
-	la a0,UnselectedMediumLevel
-	li  a1,85
+	la a0, medio
+	li  a1,120
 	li a2,130
-	jal auxiliarDrawImage
+	li a3, 185
+	jal auxiliarPrintString
 	
-	la a0,UnselectedHardLevel
-	li  a1,85
+	la a0,dificil
+	li  a1,120
 	li a2,170
-	jal auxiliarDrawImage
+	li a3, 185
+	jal auxiliarPrintString
 	
 menuLoop:				#s1 armazenará status da seleção
 	jal auxiliarReadKeyBlocking	
-	li t0, 'w'
-	li t1, 's'
-	li t2, ' '
-	li t3, 3
-	beq a0, t0, moveUp
-	beq a0,t1, moveDown
-	mv a6,s1
-	beq a0,t2,startGame
-moveUp:
-	addi s1,s1,-1
-	bge s1,zero,nonNegativeCase
-	addi s1,s1,3
-nonNegativeCase:
-	j drawMenuOptions
-moveDown:
-	addi s1,s1,1
-drawMenuOptions:				#s10 armazena o nivel da dificuldade(0 - fácil, 1 - médio, 2 - difícil)
-	rem s1,s1,t3
-	li t0,0
-	li t1,1
-	beq s1,t0,firstMenuOption
-	beq s1,t1,secondMenuOption
+	li t0, '1'
+	li t1, '2'
+	beq a0,t0,easySelected
+	beq a0,t1,mediumSelected
+				# De outra forma, hard foi selecionado
+	li a6,2
+	j startGame
 	
-	la a0,UnselectedEasyLevel	# nível difícil selecionado
-	li s10, 2 
-	li  a1,85
-	li a2,90
-	jal auxiliarDrawImage
+easySelected:
+	li a6,0
+	j startGame
 	
-	la a0,UnselectedMediumLevel
-	li  a1,85
-	li a2,130
-	jal auxiliarDrawImage
-	
-	la a0,SelectedHardLevel
-	li  a1,85
-	li a2,170
-	jal auxiliarDrawImage
-	
-	j menuLoop			# volta para loop do menu
-firstMenuOption:			# nível fácil selecionado
+mediumSelected:
+	li a6,1
 
-	la a0,SelectedEasyLevel
-	li s10, 0 
-	li  a1,85
-	li a2,90
-	jal auxiliarDrawImage
-	
-	la a0,UnselectedMediumLevel
-	li  a1,85
-	li a2,130
-	jal auxiliarDrawImage
-	
-	la a0,UnselectedHardLevel
-	li  a1,85
-	li a2,170
-	jal auxiliarDrawImage
-	j menuLoop		 # volta para loop do menu
 
-secondMenuOption:		# nível médio selcionado
-	la a0,UnselectedEasyLevel
-	li s10, 1
-	li  a1,85
-	li a2,90
-	jal auxiliarDrawImage
-	
-	la a0,SelectedMediumLevel
-	li  a1,85
-	li a2,130
-	jal auxiliarDrawImage
-	
-	la a0,UnselectedHardLevel
-	li  a1,85
-	li a2,170
-	jal auxiliarDrawImage
-	
-	j menuLoop		#volta para loop do menu
-	
-
-	
 startGame:
 	li s1,0	 	# s1 maraca pontuação do jogador
 	li s2,0		# s2 marca a pontuação da máquina
@@ -179,12 +192,15 @@ MenuCoverLoop:
 endMenuCover:
 	li  a1,0
 	li a2,10
+	lw a3, frame_zero
 	la a0,GameBackground
+	
 	jal auxiliarDrawImage
 	
 	la a0,TicTacToeStructure
 	li a1,85
 	li a2,67
+	lw a3, frame_zero
 	jal auxiliarDrawImage
 
 	jal auxiliarPrintPlacar
@@ -410,11 +426,11 @@ AIposition0:
 	beq t3, zero, inLinePos0InsertPos1
 	beq t4, zero, inLinePos0InsertPos2
 	
-	inLinePos0InsertPos1:
+inLinePos0InsertPos1:
 	li s3, 1
 	li s4, 0
 	jal setOinBoard
-	inLinePos0InsertPos2:
+inLinePos0InsertPos2:
 	li s3, 2
 	li s4, 0
 	jal setOinBoard
@@ -1043,6 +1059,8 @@ returnToMachineTurn:
 
 # A fim de evitar que os saltos não alcancem suas respectivas labels, esses campos servem como
 # ponte para alcançar as labels.
+auxiliarPrintString:
+	j printString
 
 auxiliarUserWon:
 	j userWon
@@ -1994,10 +2012,10 @@ velha:
 userWonInTotal:
 	
 	lw a3,frame_zero
-	la a0,TicTacToeCover
-	li a1,85
-	li a2,67
-	jal drawImage
+	#la a0,TicTacToeCover
+	#li a1,85
+	#li a2,67
+	#jal drawImage
 
 	la a0,VoceVenceu	
 	li a1,15
@@ -2005,10 +2023,10 @@ userWonInTotal:
 	j endGameMenu
 machineWonInTotal:
 	lw a3,frame_zero
-	la a0,TicTacToeCover
-	li a1,85
-	li a2,67
-	jal drawImage
+	#la a0,TicTacToeCover
+	#li a1,85
+	#li a2,67
+	#jal drawImage
 	
 	la a0,JuliaVenceu	
 	li a1,15	
@@ -2016,11 +2034,11 @@ machineWonInTotal:
 	lw a3,frame_zero
 	j endGameMenu
 velhaInTotal:
- 	la a0,CoverRest
- 	li a1,0
- 	li a2,0
- 	lw a3,frame_zero
- 	jal drawImage
+ #	la a0,CoverRest
+ #	li a1,0
+ #	li a2,0
+ #	lw a3,frame_zero
+ #	jal drawImage
  	
 	#colocar velha aqui
 endGameMenu:
@@ -2178,6 +2196,7 @@ readKeyNonBlocking:
 	sw t2, 12(t1)		# Escreve o caractere lido no display
 rknb_end:
 	ret
+	
 	
 changePosition:	# :void, recebe em a0 o caracter em ascii que o usuario pressionou
 	li t0,'w'
@@ -2591,7 +2610,7 @@ endEasyPick:
 	ret
 	
 GetRandomSquare:	# recebe o endereço da matriz, retorna o conteúdo de um quadrado aleatório
-	mv t2,a0	#t2 guarda o endereço da matriz
+	mv t2,a0	#t2 guarda o endereço da matriz'
 	li a7, 41	#guarda em a0 o numero pseudo aleatório gerad
 	ecall 	
 	
@@ -2602,4 +2621,123 @@ GetRandomSquare:	# recebe o endereço da matriz, retorna o conteúdo de um quadr
 	
 	lb a1,(a0)
 	ret			#a0 = endereco, a1= conteudo
+
+
+printString:	addi	sp, sp, -8			# aloca espaco
+    		sw	ra, 0(sp)			# salva ra
+    		sw	s0, 4(sp)			# salva s0
+    		mv	s0, a0              		# s0 = endereco do caractere na string
+
+loopprintString:lb	a0, 0(s0)                 	# le em a0 o caracter a ser impresso
+
+    		beq     a0, zero, fimloopprintString	# string ASCIIZ termina com NULL
+
+    		jal     printChar       		# imprime char
+    		
+		addi    a1, a1, 8                 	# incrementa a coluna
+		li 	t6, 313		
+		blt	a1, t6, NaoPulaLinha	    	# se ainda tiver lugar na linha
+    		addi    a2, a2, 8                 	# incrementa a linha
+    		mv    	a1, zero			# volta a coluna zero
+
+NaoPulaLinha:	addi    s0, s0, 1			# proximo caractere
+    		j       loopprintString       		# volta ao loop
+
+fimloopprintString:	lw      ra, 0(sp)    		# recupera ra
+			lw 	s0, 0(sp)		# recupera s0 original
+    			addi    sp, sp, 8		# libera espaco
+fimprintString:	ret      	    			# retorna
+
+
+#########################################################
+#  PrintChar                                            #
+#  a0 = char(ASCII)                                     #
+#  a1 = x                                               #
+#  a2 = y                                               #
+#  a3 = cores (0x0000bbff) 	b = fundo, f = frente	#
+#  a4 = frame (0 ou 1)					#
+#########################################################
+#   t0 = i                                             #
+#   t1 = j                                             #
+#   t2 = endereco do char na memoria                   #
+#   t3 = metade do char (2a e depois 1a)               #
+#   t4 = endereco para impressao                       #
+#   t5 = background color                              #
+#   t6 = foreground color                              #
+#########################################################
+#	t9 foi convertido para s9 pois nao ha registradores temporarios sobrando dentro desta funcao
+
+
+printChar:	li 	t4, 0xFF	# t4 temporario
+		slli 	t4, t4, 8	# t4 = 0x0000FF00 (no RARS, nao podemos fazer diretamente "andi rd, rs1, 0xFF00")
+		and    	t5, a3, t4   	# t5 obtem cor de fundo
+    		srli	t5, t5, 8	# numero da cor de fundo
+		andi   	t6, a3, 0xFF    # t6 obtem cor de frente
+
+		li 	tp, ' '
+		blt 	a0, tp, printChar.NAOIMPRIMIVEL	# ascii menor que 32 nao eh imprimivel
+		li 	tp, '~'
+		bgt	a0, tp, printChar.NAOIMPRIMIVEL	# ascii Maior que 126  nao eh imprimivel
+    		j       printChar.IMPRIMIVEL
+    
+printChar.NAOIMPRIMIVEL: li      a0, 32		# Imprime espaco
+
+printChar.IMPRIMIVEL:	li	tp, 320		# Num colunas 320
+			mul t4,tp,a2
+			j printChar.mul1d
+printChar.mul1:		mul     t4, tp, a2			# multiplica a2x320  t4 = coordenada y
+printChar.mul1d:	add     t4, t4, a1               	# t4 = 320*y + x
+			addi    t4, t4, 7                 	# t4 = 320*y + (x+7)
+			li      tp, 0xFF000000          	# Endereco de inicio da memoria VGA0
+			beq 	a4, zero, printChar.PULAFRAME		# Verifica qual o frame a ser usado em a4
+			li      tp, 0xFF100000          	# Endereco de inicio da memoria VGA1
+printChar.PULAFRAME:	add     t4, t4, tp               	# t4 = endereco de impressao do ultimo pixel da primeira linha do char
+			addi    t2, a0, -32               	# indice do char na memoria
+			slli    t2, t2, 3                 	# offset em bytes em relacao ao endereco inicial
+			la      t3, LabelTabChar		# endereco dos caracteres na memoria
+			add     t2, t2, t3               	# endereco do caractere na memoria
+			lw      t3, 0(t2)                 	# carrega a primeira word do char
+			li 	t0, 4				# i=4
+
+printChar.forChar1I:	beq     t0, zero, printChar.endForChar1I # if(i == 0) end for i
+    			addi    t1, zero, 8               	# j = 8
+
+printChar.forChar1J:	beq     t1, zero, printChar.endForChar1J # if(j == 0) end for j
+        		andi    s9, t3, 0x001			# primeiro bit do caracter
+        		srli    t3, t3, 1             		# retira o primeiro bit
+        		beq     s9, zero, printChar.printCharPixelbg1	# pixel eh fundo?
+        		sb      t6, 0(t4)             		# imprime pixel com cor de frente
+        		j       printChar.endCharPixel1
+printChar.printCharPixelbg1:	sb      t5, 0(t4)                # imprime pixel com cor de fundo
+printChar.endCharPixel1: addi    t1, t1, -1                	# j--
+    			addi    t4, t4, -1                	# t4 aponta um pixel para a esquerda
+    			j       printChar.forChar1J		# vollta novo pixel
+
+printChar.endForChar1J: addi    t0, t0, -1 		# i--
+    			addi    t4, t4, 328           	# 2**12 + 8
+    			j       printChar.forChar1I	# volta ao loop
+
+printChar.endForChar1I:	lw      t3, 4(t2)           	# carrega a segunda word do char
+			li 	t0, 4			# i = 4
+printChar.forChar2I:    beq     t0, zero, printChar.endForChar2I  # if(i == 0) end for i
+    			addi    t1, zero, 8             # j = 8
+
+printChar.forChar2J:	beq	t1, zero, printChar.endForChar2J # if(j == 0) end for j
+        		andi    s9, t3, 0x001	    		# pixel a ser impresso
+        		srli    t3, t3, 1                 	# desloca para o proximo
+        		beq     s9, zero, printChar.printCharPixelbg2 # pixel eh fundo?
+        		sb      t6, 0(t4)			# imprime cor frente
+        		j       printChar.endCharPixel2		# volta ao loop
+
+printChar.printCharPixelbg2:	sb      t5, 0(t4)		# imprime cor de fundo
+
+printChar.endCharPixel2:	addi    t1, t1, -1		# j--
+    				addi    t4, t4, -1              # t4 aponta um pixel para a esquerda
+    				j       printChar.forChar2J
+
+printChar.endForChar2J:	addi	t0, t0, -1 		# i--
+    			addi    t4, t4, 328		#
+    			j       printChar.forChar2I	# volta ao loop
+
+printChar.endForChar2I:	ret	
 	
